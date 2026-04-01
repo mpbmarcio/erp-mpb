c7ec1cfa1655f430473d849be9032847df8296a3
Márcio Pires Barboza <mpbmarcio@hotmail.com>
2026-04-01 11:09:24 -0300

    feat: adiciona FornecedorController

---
+++ src/main/java/br/com/mpb/erp/controller/FornecedorController.java
@@ -0,0 +1,24 @@
+package br.com.mpb.erp.controller;
+
+import br.com.mpb.erp.entity.Fornecedor;
+import br.com.mpb.erp.service.FornecedorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/fornecedores")
+public class FornecedorController{
+
+    @Autowired
+    private FornecedorService service;
+
+    // Endpoint GET /fornecedores
+    @GetMapping
+    public List<Fornecedor> listarFornecedores() {
+        return service.listar();
+    }
+}
