5b0a4368575ce179ebbbfcf9ce1db58137d440c3
Márcio Pires Barboza <mpbmarcio@hotmail.com>
2026-04-02 11:09:28 -0300

    feat: adiciona UFRepository

---
+++ src/main/java/br/com/mpb/erp/repository/UFRepository.java
@@ -0,0 +1,7 @@
+package br.com.mpb.erp.repository;
+
+import br.com.mpb.erp.entity.UF;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UFRepository extends JpaRepository<UF, Integer> {
+}
