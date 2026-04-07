56144e6ba5743b9b6b8a95eccff67edb745f22d2
Márcio Pires Barboza <mpbmarcio@hotmail.com>
2026-04-06 22:11:32 -0300

    feat: adiciona repository ContasReceberRepository

---
+++ src/main/java/br/com/mpb/erp/repository/ContasReceberRepository.java
@@ -0,0 +1,7 @@
+package br.com.mpb.erp.repository;
+
+import br.com.mpb.erp.entity.ContasReceber;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContasReceberRepository extends JpaRepository<ContasReceber, Integer> {
+}
