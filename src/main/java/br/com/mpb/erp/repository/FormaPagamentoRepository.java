009c6193345e792b3502103b1f967cfe30a081c4
Márcio Pires Barboza <mpbmarcio@hotmail.com>
2026-04-08 21:47:39 -0300

    feat: adiciona repository FormaPagamentoRepository

---
+++ src/main/java/br/com/mpb/erp/repository/FormaPagamentoRepository.java
@@ -0,0 +1,7 @@
+package br.com.mpb.erp.repository;
+
+import br.com.mpb.erp.entity.FormaPagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Integer> {
+}
