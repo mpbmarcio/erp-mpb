06595706de5ac74a13d24e08e8fa611a53671158
Márcio Pires Barboza <mpbmarcio@hotmail.com>
2026-04-15 22:36:33 -0300

    feat: adiciona controller ProdutoController

---
+++ src/main/java/br/com/mpb/erp/controller/ProdutoController.java
@@ -0,0 +1,23 @@
+package br.com.mpb.erp.controller;
+
+import br.com.mpb.erp.entity.Produto;
+import br.com.mpb.erp.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService service;
+
+    @GetMapping
+    public List<Produto> listar() {
+        return service.listar();
+    }
+}
